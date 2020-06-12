733f371f546ae003061d35e6024db02f1685c191
Jonas Ng <jonasngs30@gmail.com>
2020-06-12 16:28:18 +0800

    added dependencies

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
 const express = require("express");
+const cors = require('cors');
+const Joi = require('joi'); // joi is used for data validation
 
 const app = express();
+
+app.use(express.json());
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.send("hello world");
 });
 
-app.listen(3000);
+app.get('/api/testing/:id', (req, res) => {
+    res.send(req.query);
+})
+
+
+
+// PORT
+const port = process.env.PORT || 3000;
+
+app.listen(port);
