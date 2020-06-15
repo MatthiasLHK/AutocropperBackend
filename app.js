49cf32afad1e101cc06599fb4e53213937f7f8fe
MatthiasLHK <59341741+MatthiasLHK@users.noreply.github.com>
2020-06-15 13:19:12 +0800

    connect to database, added pg-promise module

--- app.js
+++ app.js
@@ -2,15 +2,22 @@ const express = require("express");
 const cors = require('cors');
 const Joi = require('joi');
 const pool = require("./database");
+const pgp = require('pg-promise')({});
+const cn = 'postgres://MatthiasLHK:autocropper123@db-autocropper-v2.czixlgpzza3d.ap-southeast-1.rds.amazonaws.com:5432/autocropperdb';
+const db = pgp(cn);
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-app.get('/', (req, res) => {
-    res.send("hello world");
+// database data
+const data = db.manyOrNone('SELECT user_id, username, password, email, created_on FROM account')
+    .then(x=>{app.get('/', (req, res) => {
+        res.send(x);
+    });
 });
+
 
 // PORT
 const port = process.env.PORT || 5000;
