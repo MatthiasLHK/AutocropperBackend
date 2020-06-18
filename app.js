db9bd386663001336f178b0c652230a0706ee6b6
MatthiasLHK <59341741+MatthiasLHK@users.noreply.github.com>
2020-06-18 18:29:01 +0800

    testing 3

--- app.js
+++ app.js
@@ -11,6 +11,8 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+console.log('test');
+
 app.get('/',(req,res)=>{
     res.send("Welcome, successfully set up remote! Matt and Jonas!");
 });
