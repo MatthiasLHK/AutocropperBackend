721f6ae2e674e24eea0d7354e46a1beed63b6dff
MatthiasLHK <59341741+MatthiasLHK@users.noreply.github.com>
2020-06-18 18:44:55 +0800

    'first'

--- app.js
+++ app.js
@@ -11,84 +11,106 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+
+app.get('',(req,res)=>{
+    res.send("Welcome, successfully set up remote! Matt and Jonas!");
+});
+
 // database data
-const data = db.manyOrNone('SELECT user_id, username, password, email, created_on FROM account')
-    .then(x=>{app.get('/profile', (req, res) => {
-        console.log(res.data);
-        res.send(x);
-    });
-});
-
-app.get('/results/:id', (req, res) => {
-    const setting_id = req.params.id;
-    db.one("SELECT * from settings where setting_id = $1", [setting_id])
-    .then(result => {
-        res.send(result);
-    })
-    .catch(error => {
-        console.log(error)
-    })
-})
-
-app.get('/profile/:user', (req, res) => {
-    const userId = req.params.user;
-    db.one("SELECT * from account where user_id = $1", [userId])
-    .then(result => {
-        res.send(result);
-    })
-    .catch(error => {
-        console.log(error)
-    })
-});
-
-app.delete('/profile/:id', async (req, res) => {
-    const { id } = req.params;
-    db.result('delete from account where user_id = $1', id)
-    .then(result => {
-        res.status(200).json({
-            status: 'success',
-            message: 'Removed user ' + id
-        });
-    })
-    .catch(error => {
-        console.log(error)
-    })
-})
-
-
-app.put('/profile/:id', (req, res) => {
-    const userId = req.params.id;
-    const username = "jonasngs";
-    db.none('update account set username=$1 where user_id=$2', [username, userId])
-    .then(result => {
-        res.send(result);
-    })
-    .catch(error => {
-        console.log(error)
-    })
-})
-
-
-app.post('/set_data', (req, res) => {
-
-    const temp = req.body.temperature;
-    const water = req.body.water;
-    const light= req.body.light;
-    const humidity= req.body.humidity;
-    const name = "corn";
-    db.none('insert into settings (plant_name, temperature, humidity, light, water)' +
-            'values($1, $2, $3, $4, $5)', [name, temp, humidity, light, water])
-            .then(result => {
-                res.status(200).json({
-                    status: 'success',
-                    message: 'added new plant'
-                });
-            })
-            .catch(error => {
-                console.log(error)
-            })
-            });
-
+// const data = db.manyOrNone('SELECT user_id, username, password, email, created_on FROM account')
+//     .then(x=>{app.get('/profile', (req, res) => {
+//         console.log(res.data);
+//         res.send(x);
+//     });
+// });
+//
+// app.get('/results/:id', (req, res) => {
+//     const setting_id = req.params.id;
+//     db.one("SELECT * from settings where setting_id = $1", [setting_id])
+//     .then(result => {
+//         res.send(result);
+//     })
+//     .catch(error => {
+//         console.log(error)
+//     })
+// })
+//
+// app.get('/profile/:user', (req, res) => {
+//     const userId = req.params.user;
+//     db.one("SELECT * from account where user_id = $1", [userId])
+//     .then(result => {
+//         res.send(result);
+//     })
+//     .catch(error => {
+//         console.log(error)
+//     })
+// });
+//
+// app.delete('/profile/:id', async (req, res) => {
+//     const { id } = req.params;
+//     db.result('delete from account where user_id = $1', id)
+//     .then(result => {
+//         res.status(200).json({
+//             status: 'success',
+//             message: 'Removed user ' + id
+//         });
+//     })
+//     .catch(error => {
+//         console.log(error)
+//     })
+// })
+//
+//
+// app.put('/profile/:id', (req, res) => {
+//     const userId = req.params.id;
+//     const username = "jonasngs";
+//     db.none('update account set username=$1 where user_id=$2', [username, userId])
+//     .then(result => {
+//         res.send(result);
+//     })
+//     .catch(error => {
+//         console.log(error)
+//     })
+// })
+//
+//
+// app.post('/set_data', (req, res) => {
+//     const temp = req.body.temperature;
+//     const water = req.body.water;
+//     const light= req.body.light;
+//     const humidity= req.body.humidity;
+//     const name = "corn";
+//     db.none('insert into settings (plant_name, temperature, humidity, light, water)' +
+//             'values($1, $2, $3, $4, $5)', [name, temp, humidity, light, water])
+//             .then(result => {
+//                 res.status(200).json({
+//                     status: 'success',
+//                     message: 'added new plant'
+//                 });
+//             })
+//             .catch(error => {
+//                 console.log(error)
+//             })
+//             });
+//
+// app.post('/login', (req, res) => {
+//     const username = req.body.username;
+//     const password = req.body.password;
+//     const email = req.body.email;
+//     const created_on = NOW();
+//     const login = null;
+//     db.none('insert into account (username, password, email, created_on, last_login)' +
+//             'values($1, $2, $3, NOW(), $4)', [username, password, email, login])
+//             .then(result => {
+//                 res.status(200).json({
+//                     status: 'success',
+//                     message: 'successfully registered'
+//                 });
+//             })
+//             .catch(error => {
+//                 console.log(error)
+//             })
+//             });
 
 
 // PORT
