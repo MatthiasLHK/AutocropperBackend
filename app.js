27490b75bec53d5af22bd4e41684c2f707a88f7f
MatthiasLHK <59341741+MatthiasLHK@users.noreply.github.com>
2020-06-18 18:46:33 +0800

    'second'

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(cors());
 
 
 app.get('',(req,res)=>{
-    res.send("Welcome, successfully set up remote! Matt and Jonas!");
+    res.send("Welcome, successfully set up remote! Matt and Jonas! Testing round 2 for editiing changes!");
 });
 
 // database data
